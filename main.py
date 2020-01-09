322b6b132904bd1678f3031132f5c5157b7d09a6
Bradley-B <brad.boxer1@gmail.com>
2020-01-09 00:15:27 -0500

    Cleanup
    Invert the dice, fixed off by one errors, get the input image through a file dialog, and create the output directory if it doesn't exist.

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from PIL import Image
+import PIL.ImageOps
 import bisect
+import tkinter
+from tkinter.filedialog import askopenfilename
+import os
 
+die_image_dimension = 42
 num_sections = 6
 base = 255 / num_sections
 midpoints = list(map(lambda x: int(x * base), range(1, num_sections + 1)))
@@ -16,36 +21,36 @@ die_map = {
 
 for key, val in die_map.items():
     die_map[key] = Image.open(val)
+    die_map[key] = die_map[key].convert(mode='L')
+    die_map[key] = PIL.ImageOps.invert(die_map[key])
 
 
 def run():
-    filename = 'morgan2.png'
+    tkinter.Tk().withdraw()
+    filename = askopenfilename()
     existing_image = Image.open(filename)
 
     gray_image = existing_image.convert(mode='L')
     width, height = gray_image.size
-    output_image = Image.new('LA', (width*128, height*128), color=150)
+    output_image = Image.new('L', (width*die_image_dimension, height*die_image_dimension), color=150)
 
-    for i in range(1, width - 1):
-        for j in range(1, height - 1):
+    for i in range(0, width):
+        for j in range(0, height):
             quantize_image, pixel = quantize(gray_image.getpixel((i, j)))
             # gray_image.putpixel((i, j), pixel)
-            output_image.paste(quantize_image, box=(i*128, j*128))
-            # print("pasting image at", i*128, j*128, quantize_image.filename)
+            output_image.paste(quantize_image, box=(i*die_image_dimension, j*die_image_dimension))
 
-    #gray_image.rotate(90)
-    # gray_image.show()
-    #output_image.show()
-    output_image = output_image.resize((8192, 10912))
-    output_image.save("output.png")
-    #new_image.save("output.png")
+    output_image.show()
+    if not os.path.exists("output"):
+        os.makedirs("output")
+    output_image.save("output/output_" + os.path.basename(filename))
 
 
 def quantize(pixel_value):
     index = bisect.bisect_right(midpoints, pixel_value)
     if index >= num_sections - 1:
         index = num_sections - 1
-    return die_map.get(index), midpoints[index]
+    return die_map.get(num_sections-index-1), midpoints[index]
 
 
 if __name__ == '__main__':
